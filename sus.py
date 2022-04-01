c523dcc5d73b0e2ecba85154a6e180bda1543dcc
YukiHasDrip <83093992+YukiHasDrip@users.noreply.github.com>
2022-04-01 15:27:44 +0200

    Create sus.py

---
+++ sus.py
@@ -0,0 +1,62 @@
+from gturtle import * 
+makeTurtle()
+counter = 0
+right(180)
+penUp()
+while counter < 100:
+    if counter < 19:
+        setPenColor("Red")
+    else:
+        setPenColor("Blue")
+    
+    if counter == 10:
+        left(90)
+    elif (counter == 19) or (counter == 28) or (counter == 36) or (counter == 44) or (counter == 51) or (counter == 58) or (counter == 64) or (counter == 70) or (counter == 75) or (counter == 80) or (counter == 84) or (counter == 88) or (counter == 91) or (counter == 94) or (counter == 96) or (counter == 98) or (counter == 99):
+        left(90)
+    forward(30)
+    dot(20)
+    counter = counter + 1
+    
+    
+---------------------------------------------------------------------
+
+from gturtle import * 
+makeTurtle()
+counter = 0
+right(180)
+penUp()
+while counter < 100:
+    if counter < 36:
+        setPenColor("Red")
+    else:
+        setPenColor("Blue")
+    
+    if counter == 10:
+        left(90)
+    elif (counter == 19) or (counter == 28) or (counter == 36) or (counter == 44) or (counter == 51) or (counter == 58) or (counter == 64) or (counter == 70) or (counter == 75) or (counter == 80) or (counter == 84) or (counter == 88) or (counter == 91) or (counter == 94) or (counter == 96) or (counter == 98) or (counter == 99):
+        left(90)
+    forward(30)
+    dot(20)
+    counter = counter + 1
+    
+    
+------------------------------------------------------------------------
+
+from gturtle import * 
+makeTurtle()
+counter = 0
+right(180)
+penUp()
+while counter < 100:
+    if (counter == 0) or (counter == 9) or (counter == 18) or (counter == 27) or (counter == 36) or (counter == 43) or (counter == 50) or (counter == 57) or (counter == 64) or (counter == 69) or (counter == 74) or (counter == 79) or (counter == 84) or (counter == 87) or (counter == 90) or (counter == 93) or (counter == 96) or (counter == 97) or (counter == 98) or (counter == 99) or (counter == 100):
+        setPenColor("Red")
+    else:
+        setPenColor("Blue")
+    
+    if counter == 10:
+        left(90)
+    elif (counter == 19) or (counter == 28) or (counter == 36) or (counter == 44) or (counter == 51) or (counter == 58) or (counter == 64) or (counter == 70) or (counter == 75) or (counter == 80) or (counter == 84) or (counter == 88) or (counter == 91) or (counter == 94) or (counter == 96) or (counter == 98) or (counter == 99):
+        left(90)
+    forward(30)
+    dot(20)
+    counter = counter + 1
